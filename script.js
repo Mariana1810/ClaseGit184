d2e897a5199e5693cad4c4bc16350c66a379b270
Marian1810 <Marian1810@hotmail.com>
2023-04-10 22:31:32 -0300

    mi comentario MM

---
+++ script.js
@@ -0,0 +1,7 @@
+let nombre = "Mariana";//esta es una variable de nombre
+console.log (nombre);// con esto se muestra en la consola el nombre
+console.log(typeof(nombre)); // con esto en la consola me muestra que tipo de valor es nombre en este caso string
+
+
+
+
